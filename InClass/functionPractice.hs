4c396624eb4e6bb3ca307b7531f3e582aea09bed
Joey Lawrance <lawrancej@wit.edu>
2013-09-25 12:04:08 -0400

    Added today's thing.

---
+++ InClass/functionPractice.hs
@@ -0,0 +1,11 @@
+-- Practice problems
+
+-- Here's one way to write sum
+sum' = foldl (+) 0
+
+-- Write these functions.
+max' :: [Integer] -> Integer
+
+min' :: [Integer] -> Integer
+
+average :: [Integer] -> Double
---
+++ Labs/Lab2.md
@@ -0,0 +1,3 @@
+# Lab 2: More practice with Haskell
+
+Rewrite lab 1 functions using different features of Haskell.
