1bdb71e2600909d0ca45919dc76334d4865e08bf
Joey Lawrance <lawrancej@wit.edu>
2013-09-30 12:02:18 -0400

    Nicer type signature.

--- InClass/partialfunctions.hs
+++ InClass/partialfunctions.hs
@@ -6,7 +6,7 @@ dup :: [a] -> [a]
 
 -- Given a number, add the number to itself
 -- E.g., times_two 2 should produce 4
-times_two :: Integer -> Integer
+times_two :: Num a => a -> a
 
 -- Create a function called square
 -- Given a function that takes two inputs, and a single value, it applies
