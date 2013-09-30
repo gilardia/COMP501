e9618493c6301d7cc4e94a96bf45dfb1f7c7a3d3
Joey Lawrance <lawrancej@wit.edu>
2013-09-30 11:53:50 -0400

    Added today's inclass activity.

---
+++ InClass/partialfunctions.hs
@@ -0,0 +1,15 @@
+-- In Class activity
+
+-- Given a list, concatenate the list with itself
+-- E.g., dup [1,2,3] should produce [1,2,3,1,2,3]
+dup :: [a] -> [a]
+
+-- Given a number, add the number to itself
+-- E.g., times_two 2 should produce 4
+times_two :: Integer -> Integer
+
+-- Create a function called square
+-- Given a function that takes two inputs, and a single value, it applies
+-- the function on an that value twice.
+-- E.g., we could re-implement dup above as dup = square (++)
+square :: (a -> a -> b) -> a -> b
