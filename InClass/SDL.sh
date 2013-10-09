c74293f23d30e2d5fde1dd383bdfabbb2d06d29e
Joey Lawrance <lawrancej@wit.edu>
2013-10-09 01:56:36 -0400

    Cleaned up SDL script.

--- InClass/SDL.sh
+++ InClass/SDL.sh
@@ -7,6 +7,20 @@
 
 # FIXME: Support SDL-gfx (currently downloads it, but nothing else)
 
+# Determine CPU
+if [[ $PROCESSOR_ARCHITECTURE == "x86" ]]; then
+CPU="x86"
+else
+CPU="x64"
+fi
+
+# SDL folders
+SDL=/c/SDL
+BIN=$SDL/bin
+LIB=$SDL/lib
+INCLUDE=$SDL/include
+PATH=$PATH:$BIN:$LIB
+
 # This function reads URLs from standard in, then downloads and extracts them.
 # It accepts a parameter to specify the directory to extract to.
 function download_and_extract {
@@ -22,14 +36,57 @@ function download_and_extract {
 	done
 }
 
-# Get SDL binaries for your system. Store in C:\SDL
-download_and_extract /c/SDL <<URL
+# This function makes patches to Haskell SDL bindings.
+function make_patch {
+cd $1
+lower=`printf $2 | tr [:upper:] [:lower:]`
+patch -p1 <<PATCH
+--- a/Graphics/UI/SDL/$2/Version.hsc
++++ b/Graphics/UI/SDL/$2/Version.hsc
+@@ -1,4 +1,8 @@
+ #include "SDL_$lower.h"
++#include "SDL.h"
++#ifdef main
++#undef main
++#endif
+ module Graphics.UI.SDL.$2.Version
+     ( compiledFor
+     , linkedWith
+
+PATCH
+cd ..
+}
+
+# Get SDL binaries for your system. Store in SDL folder
+if [[ ! -e $SDL ]]; then
+download_and_extract $SDL <<URL
 http://www.libsdl.org/release/SDL-devel-1.2.15-mingw32.tar.gz
 http://www.libsdl.org/projects/SDL_ttf/release/SDL_ttf-devel-2.0.11-VC.zip
 http://www.libsdl.org/projects/SDL_image/release/SDL_image-devel-1.2.12-VC.zip
-http://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-devel-1.2.12-VC.zip
 http://www.ferzkopp.net/Software/SDL_gfx-2.0/SDL_gfx-2.0.24.tar.gz
+http://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-devel-1.2.12-VC.zip
 URL
+
+# Move disparate SDL libs/includes to one folder
+mkdir -p $LIB
+mkdir -p $INCLUDE
+mkdir -p $BIN
+
+# Copy main SDL to standard location
+cp $SDL/SDL-1.2.15/bin/sdl-config $BIN
+cp $SDL/SDL-1.2.15/bin/SDL.dll $LIB
+cp $SDL/SDL-1.2.15/lib/* $LIB
+cp --recursive $SDL/SDL-1.2.15/include/* $INCLUDE
+
+# Copy add-on libraries to standard location
+for dir in $SDL/SDL_* ; do
+	cp --recursive $dir/include/* $INCLUDE 2> /dev/null
+	cp --recursive $dir/lib/$CPU/* $LIB 2> /dev/null
+done
+
+rm -rf $SDL/SDL*
+fi
+
 
 # Get Haskell SDL bindings. Store temporarily in current directory.
 download_and_extract . <<URL
@@ -40,122 +97,65 @@ http://hackage.haskell.org/package/SDL-mixer-0.6.1/SDL-mixer-0.6.1.tar.gz
 http://hackage.haskell.org/package/SDL-gfx-0.6.0/SDL-gfx-0.6.0.tar.gz
 URL
 
+# Got to patch add-on libraries for Windows
+make_patch SDL-image-0.6.1 Image
+make_patch SDL-mixer-0.6.1 Mixer
+make_patch SDL-ttf-0.6.2 TTF
+
 # Install Haskell SDL binding
-PATH=$PATH:/c/SDL/SDL-1.2.15/bin
-cd SDL-0.6.5
-runghc Setup.lhs configure \
---extra-include-dirs=/c/SDL/SDL-1.2.15/include/SDL \
---extra-lib-dirs=/c/SDL/SDL-1.2.15/lib
-runghc Setup.lhs build
-runghc Setup.lhs install
+for dir in SDL-*; do
+	cd $dir
+	runghc Setup.lhs configure --extra-include-dirs=$INCLUDE --extra-include-dirs=$INCLUDE/SDL --extra-lib-dirs=$LIB
+	runghc Setup.lhs build
+	runghc Setup.lhs install
+	cd ..
+done
 
-# Install Haskell SDL-image binding
-cd ..
-cd SDL-image-0.6.1
-patch -p1 <<PATCH
---- a/Graphics/UI/SDL/Image/Version.hsc
-+++ b/Graphics/UI/SDL/Image/Version.hsc
-@@ -1,4 +1,8 @@
- #include "SDL_image.h"
-+#include "SDL.h"
-+#ifdef main
-+#undef main
-+#endif
- module Graphics.UI.SDL.Image.Version
-     ( compiledFor
-     , linkedWith
-
-PATCH
-runghc Setup.lhs configure \
---extra-include-dirs=/c/SDL/SDL_image-1.2.12/include
-runghc Setup.lhs build
-runghc Setup.lhs install
-
-cd ..
-cd SDL-mixer-0.6.1
-patch -p1 <<PATCH
---- a/Graphics/UI/SDL/Mixer/Version.hsc
-+++ b/Graphics/UI/SDL/Mixer/Version.hsc
-@@ -1,4 +1,8 @@
- #include "SDL_mixer.h"
-+#include "SDL.h"
-+#ifdef main
-+#undef main
-+#endif
- module Graphics.UI.SDL.Mixer.Version
-     ( compiledFor
-     , linkedWith
-
-PATCH
-runghc Setup.lhs configure \
---extra-include-dirs=/c/SDL/SDL_mixer-1.2.12/include
-runghc Setup.lhs build
-runghc Setup.lhs install
-
-
-cd ..
-cd SDL-ttf-0.6.2
-patch -p1 <<PATCH
---- a/Graphics/UI/SDL/TTF/Version.hsc
-+++ b/Graphics/UI/SDL/TTF/Version.hsc
-@@ -1,4 +1,8 @@
- #include "SDL_ttf.h"
-+#include "SDL.h"
-+#ifdef main
-+#undef main
-+#endif
- module Graphics.UI.SDL.TTF.Version
-     ( compiledFor
-     , linkedWith
-
-PATCH
-runghc Setup.lhs configure \
---extra-include-dirs=/c/SDL/SDL_ttf-2.0.11/include
-runghc Setup.lhs build
-runghc Setup.lhs install
-
-if [[ $PROCESSOR_ARCHITECTURE == "x86" ]]; then
-CPU="x86"
-else
-CPU="x64"
-fi
-
-cp /c/SDL/SDL_image-1.2.12/lib/$CPU/*.dll /c/SDL/SDL-1.2.15/bin
-cp /c/SDL/SDL_mixer-1.2.12/lib/$CPU/*.dll /c/SDL/SDL-1.2.15/bin
-cp /c/SDL/SDL_ttf-2.0.11/lib/$CPU/*.dll /c/SDL/SDL-1.2.15/bin
+exit
 
 cd ..
 rm -rf SDL-* # Clean up Haskell bindings
 
-printf ";C:\\SDL\\SDL-1.2.15\\\bin" > /dev/clipboard
-echo 
-echo
-echo "Haskell SDL installation ALMOST COMPLETE."
-echo "You need to add C:\\SDL\\SDL-1.2.15\\bin to the System Path"
-echo
-echo "I have already copied that path to the clipboard."
-echo
-echo "Step-by-step:"
-echo "1. Go to Start -> Computer -> System properties -> Advanced system settings -> Environment variables..."
-echo "2. Under system variables, select Path and click Edit..."
-echo "3. Press the right arrow key to make sure nothing is selected."
-echo "4. Press Ctrl-V. If you are certain that you added to the path, click OK."
-echo "5. Come back here and press enter. More instructions await"
+printf ";C:\\SDL\\lib" > /dev/clipboard
+
+cat <<INSTRUCTIONS
+
+
+The Haskell SDL installation is ALMOST complete.
+You need to add C:\SDL\lib to the System Path.
+
+Lucky you, I have already copied that path to the clipboard.
+
+Here's what you need to do, step-by-step:
+
+1. Go to Start -> Computer -> System properties -> Advanced system settings
+2. Select Environment variables...
+3. Under system variables, select Path and click Edit...
+4. Press the right arrow key to make sure nothing is selected.
+5. Press Ctrl-V. If you are certain that you added to the path, click OK.
+6. Come back here and press enter. More instructions await.
+INSTRUCTIONS
+
+echo "You did add C:\\SDL\\lib to the System path, right?"
 read
 
-echo "I am cloning a tutorial for using SDL with Haskell."
-echo "You can get to it by doing:"
-echo
-echo "    cd ~/LazyFooHaskell"
-echo
-echo "You can build each lesson by doing:"
-echo
-echo "    cd lessonXX"
-echo "    ghc lessonXX.hs"
-echo
-echo "And then type:"
-echo
-echo "    lessonXX.exe"
+cat <<INSTRUCTIONS
+Okay, I was just checking.
+
+All right, now I am cloning a tutorial for using SDL with Haskell.
+You can get to it by doing:
+
+    cd ~/LazyFooHaskell
+
+You can build each lesson by doing:
+
+    cd lessonXX
+    ghc lessonXX.hs
+
+And then type:
+
+    lessonXX.exe
+INSTRUCTIONS
 
 cd
 git clone https://github.com/snkkid/LazyFooHaskell.git
