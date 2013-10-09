24b51192a1e068505738db9636da4b51a683d131
Joey Lawrance <lawrancej@wit.edu>
2013-10-09 10:33:41 -0400

    Added checkpoint.

--- InClass/SDL.sh
+++ InClass/SDL.sh
@@ -2,9 +2,9 @@
 
 # Adapted from: http://www.animal-machine.com/blog/2010/04/a-haskell-adventure-in-windows/
 
-# To run this, you must exit Haskell, if you are in it.
-# You must run this by running Git Bash as administrator.
-# In the start menu, right click on Git Bash -> Run As Administrator.
+# To run this, you must:
+# 1. Exit Haskell, if it is running.
+# 2. Running Git Bash as administrator. Start -> right click on Git Bash -> Run As Administrator.
 
 # FIXME: Support for SDL-mixer, SDL-ttf.
 # Something's wrong with those libraries on Windows.
@@ -20,9 +20,10 @@ fi
 # SDL folders
 SDL=/c/SDL
 BIN=$SDL/bin
-LIB="`which ghc | sed -e s/ghc//`" #$SDL/lib
+LIB=$SDL/lib
+INSTALL="`which ghc | sed -e s/ghc//`" # Where to install stuff
 INCLUDE=$SDL/include
-PATH=$PATH:$BIN:$LIB
+PATH=$PATH:$BIN # :$LIB
 
 # This function reads URLs from standard in, then downloads and extracts them.
 # It accepts a parameter to specify the directory to extract to.
@@ -60,8 +61,8 @@ PATCH
 cd ..
 }
 
-# Get SDL binaries for your system. Store in SDL folder
-if [[ ! -e $SDL ]]; then
+# Download, extract, and organize SDL binaries and bindings
+if [[ ! -e $SDL/.git ]]; then
 download_and_extract $SDL <<URL
 http://www.libsdl.org/release/SDL-devel-1.2.15-mingw32.tar.gz
 http://www.libsdl.org/projects/SDL_ttf/release/SDL_ttf-devel-2.0.11-VC.zip
@@ -70,29 +71,24 @@ http://www.ferzkopp.net/Software/SDL_gfx-2.0/SDL_gfx-2.0.24.tar.gz
 http://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-devel-1.2.12-VC.zip
 URL
 
-# Move disparate SDL libs/includes to one folder
-mkdir -p "$LIB"
+# Move disparate SDL bin/libs/includes to one folder
+mkdir -p $LIB
 mkdir -p $INCLUDE
 mkdir -p $BIN
 
 # Copy main SDL to standard location
 cp $SDL/SDL-1.2.15/bin/sdl-config $BIN
-cp $SDL/SDL-1.2.15/bin/SDL.dll "$LIB"
-cp $SDL/SDL-1.2.15/lib/* "$LIB"
+cp $SDL/SDL-1.2.15/bin/SDL.dll $LIB
+cp $SDL/SDL-1.2.15/lib/* $LIB
 cp --recursive $SDL/SDL-1.2.15/include/* $INCLUDE
 
 # Copy add-on libraries to standard location
 for dir in $SDL/SDL_* ; do
 	cp --recursive $dir/include/* $INCLUDE 2> /dev/null
-	cp --recursive $dir/lib/$CPU/* "$LIB" 2> /dev/null
+	cp --recursive $dir/lib/$CPU/* $LIB 2> /dev/null
 done
 
-rm -rf $SDL/SDL*
-fi
-
-
-# Get Haskell SDL bindings. Store temporarily in current directory.
-download_and_extract . <<URL
+download_and_extract $SDL/haskell <<URL
 http://hackage.haskell.org/package/SDL-0.6.5/SDL-0.6.5.tar.gz
 http://hackage.haskell.org/package/SDL-ttf-0.6.2/SDL-ttf-0.6.2.tar.gz
 http://hackage.haskell.org/package/SDL-image-0.6.1/SDL-image-0.6.1.tar.gz
@@ -100,23 +96,35 @@ http://hackage.haskell.org/package/SDL-mixer-0.6.1/SDL-mixer-0.6.1.tar.gz
 http://hackage.haskell.org/package/SDL-gfx-0.6.0/SDL-gfx-0.6.0.tar.gz
 URL
 
+# rm -rf $SDL/SDL*
+
+cd $SDL/haskell
+
 # Got to patch add-on libraries for Windows
 make_patch SDL-image-0.6.1 Image
 make_patch SDL-mixer-0.6.1 Mixer
 make_patch SDL-ttf-0.6.2 TTF
 
+# Take a snapshot of the work we did for debugging.
+cd ..
+git init
+git add bin haskell include lib 2> /dev/null
+git commit -m "Checkpoint." > /dev/null
+fi
+
+# Install SDL
+cp $LIB/* "$INSTALL"
+
 # Install Haskell SDL binding
+cd $SDL/haskell
+
 for dir in SDL-*; do
 	cd $dir
-	runghc Setup.lhs configure --extra-include-dirs=$INCLUDE --extra-include-dirs=$INCLUDE/SDL --extra-lib-dirs="$LIB"
+	runghc Setup.lhs configure --extra-include-dirs=$INCLUDE --extra-include-dirs=$INCLUDE/SDL --extra-lib-dirs="$INSTALL"
 	runghc Setup.lhs build
 	runghc Setup.lhs install
 	cd ..
 done
-
-# Clean up
-rm -rf SDL-*
-rm -rf $SDL
 
 # Haskell SDL tutorial
 cd
