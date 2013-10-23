d34205d5a00d4abff9bafdb48bf27ee6f3d9849a
Joey Lawrance <lawrancej@wit.edu>
2013-10-23 11:56:51 -0400

    Added skeleton tictactoe.

--- InClass/tictactoe.hs
+++ InClass/tictactoe.hs
@@ -1,8 +1,13 @@
+module Main (Cell(Blank, X, O), Game, initialGame, takeTurn, play, main) where
 -- Tic Tac Toe
 -- ghc the_name_of_this_file.hs
 -- the_name_of_this_file.exe
 
 -- Define a tic-tac-toe board type
+
+import qualified Data.Map as Map
+import Data.List
+-- analogous to import in Java
 
 data Cell = Blank | X | O -- One of 3 possible cell types. it's sorta like an enum (e.g., enum { Blank, X, O })
 
@@ -12,53 +17,69 @@ instance Show Cell where -- sorta like implementing an interface, (e.g., Cell im
 	show O = "O"
 
 -- Initialize board
--- initialBoard = [[Blank, Blank, Blank], [Blank, Blank, Blank], [Blank, Blank, Blank]]
--- initialBoard = [row, row, row] where row = take 3 $ repeat Blank -- this is just a constant to declare a blank board.
-initialBoard = replicate 3 $ replicate 3 Blank
+initialBoard = Map.fromList [((i,j), Blank) | i <- [0..2], j <- [0..2]]
 
 data Game = Game { -- sorta like a struct (e.g., struct Game { Cell[3][3] board; Cell player;})
-	board :: [[Cell]],
+	board :: Map.Map (Int, Int) Cell,
 	player :: Cell
 }
 
+initialGame = Game initialBoard X
+
 instance Show Game where -- sorta like implementing an interface, (e.g., Game implements Show)
 	show Game { board=b, player=p } = "It's player " ++ show p ++ "'s turn\n" ++
-		(foldl1 rows $ map (foldl1 cols . map show) b)
+		(foldl1 rows $ map (foldl1 cols . map show) $ unMap b)
 	 where
 	 	rows x y = x ++ "\n-----------\n" ++ y
 	 	cols x y = x ++ " | " ++ y
 
-initialGame = Game initialBoard X
+unMap :: Map.Map (Int, Int) Cell -> [[Cell]]
+unMap board = map (map snd) $ groupBy (\((i,j),_) ((k,l),_) -> i == k) $ Map.assocs board
 
-rows :: Map.Map (Int, Int) Cell -> [[Cell]]
-
-{-
 takeTurn :: Game -> (Int, Int) -> Game
 takeTurn game@Game { board=b, player=p } (i, j)
-	| validMove b i j = game -- this is not right -- guard is like an if sorta
-	| otherwise = game -- the game doesn't change
+	| validMove b i j = newGame -- if we can make a valid move at row i, column j, then return the game state
+	| otherwise = game -- otherwise, the game state doesn't change
+ where
+ 	newGame = Game {
+ 		board = b, -- FIXME might want to use Map.adjust here to change the board,
+ 		player = p -- FIXME might want to switch the player if the player didn't just win.
+	}
 
-validMove :: [[Cell]] -> Int -> Int
-validMove board i j
-	| 
-	| 
-	| 
--}
-
-takeTurn :: Game -> Int -> Int -> Game
-
-switch :: Cell -> Cell -- switch players
+switch :: Cell -> Cell -- switch players: x becomes o, o becomes x
+switch cell = Blank -- FIXME
 
 validMove :: Map.Map (Int, Int) Cell -> Int -> Int -> Bool -- is the move valid?
+-- well, when is a move invalid?
+-- when the number is outside the bounds of the board
+-- when the cell has already been played (it's not blank)
+validMove board i j = False -- FIXME
 
 diagonals :: Map.Map (Int, Int) Cell -> [[Cell]] -- get a list of diagonals
+diagonals board = [[]] -- FIXME
 
 winner :: Map.Map (Int, Int) Cell -> Bool -- is there a winner?
+winner board = False -- FIXME
 
 gameOver :: Map.Map (Int, Int) Cell -> Bool -- is the game over? either somebody won, or there's no blanks left
+gameOver board = False -- FIXME
 
 play :: Game -> IO Game
+play game = do
+	print game
+	putStrLn "Make your move"
+	row <- getChar
+	getChar -- eat the space
+	col <- getChar
+	getLine -- eat the line
+	let i = read (row:"")::Int
+	let j = read (col:"")::Int
+	let game' = takeTurn game (i,j)
+	if not $ gameOver $ board $ game' then
+		play game'
+	else do
+		print game'
+		return (game')
 
 
-main = do -- pretty much our main function
-	print initialGame
+main = play initialGame
